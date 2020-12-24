e21a52d7493ae1c2ff0bae49896ea6dc0128d845
Sarah <sarah.gh301377@gmail.com>
2020-12-24 13:20:11 -0800

    stochastic gradient descent

--- SGD/sgd.py
+++ SGD/sgd.py
@@ -16,7 +16,7 @@ def predict(X, W):
     return preds
     
 def next_batch(X, y,batchSize):
-    for i in np.arrange(0, X.shape[0], batchSize):
+    for i in np.arange(0, X.shape[0], batchSize):
         yield (X[i:i + batchSize], y[i:i + batchSize])
 
 # construct the argument parse and parse the arguments
@@ -40,3 +40,43 @@ W = np.random.randn(X.shape[1], 1)
 losses = [] 
 
 # loop over epochs
+for epoch in np.arange(0, args["epochs"]):    
+    epochloss=[]
+    # loop over batches
+    for (batchX, batchY) in next_batch(X, y, args["batch_size"]):
+        #For each batch,compute the dot product between the batch and W
+        preds = sigmond_activation(batchX.dot(W))
+        error = preds - batchY
+        epochloss.append(np.sum(error ** 2))
+        '''the gradient descent update is the dot product between our
+           current batch and the error on the batch'''
+        gradient = batchX.T.dot(error)
+        # update W
+        W += -args['alpha'] * gradient
+    # update loss history by taking the average loss across all batches
+    loss = np.average(epochloss)
+    losses.append(loss)
+    # check to see if an update should be displayed
+    if epoch == 0 or epoch % 5 == 0:
+        print("[INFO] epoch={}, loss={:.7f}".format(int(epoch + 1), loss))
+
+# evaluate our model
+print("[INFO] evaluating...")
+preds = predict(testX, W)
+print(classification_report(testY, preds))
+
+# plot data
+plt.style.use('ggplot')
+plt.figure()
+plt.title("Data")
+# c->marker colors   s->marker size in points**2
+plt.scatter(testX[:, 0], testX[:, 1], marker="o", c=testY, s=30)
+# plot loss
+plt.style.use('ggplot')
+plt.figure()
+plt.plot(np.arange(0, args["epochs"]), losses)
+plt.title("Training Loss")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss")
+plt.show()
+
