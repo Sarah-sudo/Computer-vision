16cba3099cb8df64a88372a85dce176369c99d10
Sarah <sarah.gh301377@gmail.com>
2021-01-05 13:08:03 -0800

    Implementing the Perceptron in Python

--- Perceptron/pyimagesearch/nn/perceptron.py
+++ Perceptron/pyimagesearch/nn/perceptron.py
@@ -15,3 +15,30 @@ class Perceptron:
     def fit(X, y, epochs=10):
         # applying the bias trick bias trick
         X = np.c_[X, np.ones((X.shape[0]))]
+        # actual training procedure
+         # loop over epoches
+        for epoch in np.arange(0, epochs):
+        # loop over each individual data point
+            for (x, target) in zip(X, y):
+                '''take the "dot" product between the input features
+                and the weight matrix, then pass this value
+                through the "step function" to obtain the prediction'''
+                p = self.step(np.dot(x, self.W))
+                '''only perform a weight update if our prediction
+                 does not match the target'''
+                if p != target:
+                    # determine the error
+                    error = p - target
+                    # update the weight matrix
+                    self.W += -self.alpha * error * x
+    # predict the class labels
+    def predict(self, X, addBias=True):
+        # ensure our input is a matrix
+        X = np.atleast_2d(x)
+        # check to see if the bias column should be added
+        # bias trick
+        if addBias:
+            X = np.c_[X, np.ones((X.shape[0]))]
+        return self.step(np.dot(X, self.W))
+
+
