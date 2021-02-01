7462bd5d71f6e75b4778b14499b97efcd447dc3b
Sarah <sarah.gh301377@gmail.com>
2021-02-01 15:04:42 -0800

    cifar10 by Keras

--- CIFAR10/keras_cifar10.py
+++ CIFAR10/keras_cifar10.py
@@ -16,8 +16,8 @@ print("[INFO] loading CIFAR-10 data...")
 trainX =trainX.astype("float") / 255.0
 testX = testX.astype("float") / 255.0
 # rehsape 
-trainX = trainX.rehsape((trainX.shape[0], 3072))
-testX = testX.rehsape((testX.shape[0], 3072))
+trainX = trainX.reshape((trainX.shape[0], 3072))
+testX = testX.reshape((testX.shape[0], 3072))
 
 # convert labels
 lb = LabelBinarizer()
@@ -26,3 +26,34 @@ testY = lb.transform(testY)
 # initialize the label names
 labelNames = ["airplane", "automobile", "bird", "cat", "deer", 
     "dog", "frog", "horse", "ship", "truck"]
+
+# model architecture
+model = Sequential()
+model.add(Dense(1024, input_shape=(3072,), activation='relu'))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+# train
+print("[INFO] training network...")
+sgd = SGD(0.1)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+H = model.fit(trainX, trainY, validation_data=(testX, testY), epochs=100, batch_size=32)
+
+# evaluate
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1),
+   predictions.argmax(axis=1), target_names=labelNames))
+   
+# plot
+plt.style.use('ggplot')
+plt.figure()
+plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_acc")
+plt.title('Training Loss and Accuracy')
+plt.xlabel('Epochs #')
+plt.ylabel('Loss/Accuracy')
+plt.legend()
+plt.savefig(args['output'])
