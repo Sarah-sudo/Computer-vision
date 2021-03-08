b686862cf04796c3954d9ac8aec72bd22e5018d6
Sarah <sarah.gh301377@gmail.com>
2021-03-07 18:30:23 -0800

    3-classes classification by a simple cnn

---
+++ my_project/Implementing ShallowNet/shallownet_animals.py
@@ -0,0 +1,61 @@
+# packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from pyimagesearch.preprocessing.imagetoarraypreprocessor import ImageToArrayPreprocessor
+from pyimagesearch.preprocessing.simplepreprocessor import SimplePreprocessor
+from pyimagesearch.datasets.simpledatasetloader import SimpleDatasetLoader
+from pyimagesearch.nn.conv.shallownet import ShallowNet
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+# arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, help="path to input dataset")
+args = vars(ap.parse_args())
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+# image preprocessors
+sp = SimplePreprocessor(32, 32)
+iap = ImageToArrayPreprocessor()
+sdl = SimpleDatasetLoader(preprocessors=[sp, iap])
+(data, labels) = sdl.load(imagePaths, verbose=500)
+data = data.astype("float") / 255.0
+
+# splitting the data
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
+# coverting the labels
+lb = LabelBinarizer()
+trainY = lb.fit_transform(trainY)
+testY = lb.transform(testY)
+
+print("[INFO] compiling model...")
+opt = SGD(learning_rate=0.003)
+model = ShallowNet.build(width=32, height=32, depth=3, classes=3)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+model.summary()
+# train
+print("[INFO] training network...")
+H = model.fit(trainX, trainY, validation_data=(testX, testY), batch_size=32, epochs=100, verbose=1)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=32)
+print(classification_report(testY.argmax(axis=1), predictions.argmax(axis=1), target_names=["cat", "dog", "panda"]))
+
+# plot loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 100), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 100), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 100), H.history["accuracy"], label="train_accuracy")
+plt.plot(np.arange(0, 100), H.history["val_accuracy"], label="val_accuracy")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch number")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.show()
